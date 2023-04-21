721215abda21e1b843b6600aa84d294ee2559b00
Tushar <tusharvn13@gmail.com>
2023-04-21 18:36:54 +0530

    Added demo2 testcase

--- src/JavaProject/Test1.java
+++ src/JavaProject/Test1.java
@@ -6,10 +6,15 @@ public class Test1 {
 	public void demo1() {
 		System.out.println("This is demo1 test");
 	}
+	
+	public void demo2() {
+		System.out.println("This is demo2 test");
+	}
 
 	public static void main(String[] args) {
 		Test1 ref1 = new Test1();
 		ref1.demo1();
+		ref1.demo2();
 		
 
 	}
