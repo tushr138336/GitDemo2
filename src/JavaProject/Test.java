b300dfe98ba895440ea0fe42fcb11adb602d82d5
tushar nagargoje <tushar138336@gmail.com>
2023-04-21 17:57:41 +0530

    First commit

---
+++ src/JavaProject/Test.java
@@ -0,0 +1,16 @@
+package JavaProject;
+
+public class Test {
+	
+	
+	public void demo() {
+		System.out.println("this is demo method");
+	}
+
+	public static void main(String[] args) {
+	
+		Test ref = new Test();
+		ref.demo();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HOME
+ *
+ */
+module GitDemo2 {
+}
